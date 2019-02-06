f727c99423af94f823e4768c34fa86fa127619c3
kotaro <kotaro@Kotaro-MBP13.local>
2019-02-06 17:30:59 +0900

    First commit

---
+++ judge.py
@@ -0,0 +1,30 @@
+import train as train
+import sys, os
+from PIL import Image
+import numpy as np
+import pandas as pd
+#import tensorflow as tf
+
+def evaluation(target_image_path, hdf5_path):
+        image_size = 50
+        input_dir = 'data'
+        artistname = [name for name in os.listdir(input_dir) if name != ".DS_Store"]
+        X = []
+        img = Image.open(target_image_path)
+        img = img.convert("RGB")
+        img = img.resize((image_size, image_size))
+        in_data = np.asarray(img)
+        X.append(in_data)
+
+        X = np.array(X)
+
+        model = train.TrainModel().train(X.shape[1:])
+        model.load_weights("./model/artist-model.hdf5")
+        predict = model.predict(X)
+
+#predictの中で一番大きい値のラベルを返す
+        y = predict.argmax()
+        return (artistname[y], target_image_path)
+
+if __name__ == '__main__':
+  evaluation('./testdata/img_30.jpg', './model/artist-model.hdf5')
