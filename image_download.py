5f5e26ce4d9f6bbc1df44e4d9c5bceccdcc922e2
KotaroIwasawa <kotaro1988@gmail.com>
2020-07-19 20:29:12 +0900

    Fix parser and delete unused packages

--- image_download.py
+++ image_download.py
@@ -1,3 +1,4 @@
+# from sys import argv
 import bs4
 import urllib.request
 import urllib.error
@@ -7,30 +8,21 @@ import sys
 import json
 
 # -s: Google Imagesにかける検索キーワード、複数可 (デフォルト "tokyo")
-# -n: ダウンロードする画像の数量 (デフォルト 10枚)
-# -o: 画像の保存先 (デフォルト　<DEFAULT_SAVE_DIRECTORY>で指定する)
-
+# -n: ダウンロードする画像の数量
+# -o: 画像の保存先
 
 def get_soup(url, header):
     return bs4.BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url, headers=header)), "html.parser")
 
-
-def main(args):
-    parser = argparse.ArgumentParser(description="Options for scraping Google images")
-    parser.add_argument("-s", "--search", default="tokyo", type=str, help="search term")
-    parser.add_argument("-n", "--num_images", default=10, type=int, help="num of images to scrape")
-    parser.add_argument("-o", "--directory", default="<DEFAULT_SAVE_DIRECTORY>", type=str, help="output directory")
-    args = parser.parse_args()
-
-    query = args.search.split()
+def main(search, num_images, directory):
+    query = search.split()
     query = "+".join(query)
-    max_images = args.num_images
 
     save_directory = args.directory + "/" + query
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
 
-    url = "htps://www.google.co.jp/search?q=" + query + "&source=lnms&tbm=isch"
+    url = "https://www.google.co.jp/search?q=" + query + "&source=lnms&tbm=isch"
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
     }
@@ -40,7 +32,8 @@ def main(args):
     for a in soup.find_all("div", {"class": "rg_meta"}):
         link, Type = json.loads(a.text)["ou"], json.loads(a.text)["ity"]
         ActualImages.append(link, Type)
-    for i, (img, Type) in enumerate(ActualImages[0:max_images]):
+
+    for i, (img, Type) in enumerate(ActualImages[0:num_images]):
         try:
             Type = Type if len(Type) > 0 else "jpg"
             print("Downloading image {} ({}), type is {}".format(i, img, Type))
@@ -54,10 +47,13 @@ def main(args):
 
 
 if __name__ == "__main__":
-    from sys import argv
-
+    parser = argparse.ArgumentParser(description="Options for scraping Google images")
+    parser.add_argument("-s", "--search", default="tokyo", type=str, help="search term")
+    parser.add_argument("-n", "--num_images", default=10, type=int, help="num of images to scrap")
+    parser.add_argument("-o", "--directory", default="./", type=str, help="output directory")
+    args = parser.parse_args()
     try:
-        main(argv)
+        main(search=args.search, num_images=args.num_images, directory=args.directory)
     except KeyboardInterrupt:
         pass
     sys.exit()
