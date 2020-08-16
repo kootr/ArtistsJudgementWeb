1209bf7fd3992bee348189a027505e0c8c3b2a21
KotaroIwasawa <kotaro1988@gmail.com>
2020-08-16 22:40:01 +0900

    Displays the predicted probability of each of the top three artists

--- web.py
+++ web.py
@@ -37,24 +37,20 @@ def upload_file():
             file_name = secure_filename(file.filename)
             file_path = os.path.join(app.config["UPLOAD_FOLDER"], file_name)
             file.save(file_path)
-
-            # judge.pyへアップロードされた画像を渡す
-            (
-                h_indexes,
-                artistname,
-                result_score,
-                target_image_path,
-            ) = judge_artist_name.main(file_path, "./model/artist-model_master.hdf5")
+            (h_indexes, artistname, result_score) = judge_artist_name.main(
+                file_path, "./model/artist-model_master.hdf5"
+            )
+        else:
+            return redirect(url_for("index"))
 
         return render_template(
             "index.html",
             h_indexes=h_indexes,
             artistname=artistname,
             result_score=result_score,
-            target_image_path=file_path,
+            file_path=file_path,
         )
     else:
-        # エラーなどでリダイレクトしたい場合
         return redirect(url_for("index"))
 
 
