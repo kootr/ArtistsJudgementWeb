d024ab51f420338496a082c65246511d5f17c71f
KotaroIwasawa <kotaro1988@gmail.com>
2020-08-23 14:20:15 +0900

    Change image size 50 -> 200 and the way of augumentation

--- npy_generate.py
+++ npy_generate.py
@@ -5,45 +5,42 @@ import numpy as np
 import math
 import random
 
-image_size = 50
-num_traindata = 500
+image_size = 200
+num_data = 300
 Image.LOAD_TRUNCATED_IMAGES = True
 
 
 def main():
     train_data = []
     artistname = []
-    input_dir = "images"
-    dir_list = os.listdir(input_dir)
-    for dir_name in dir_list:
-        if dir_name == "upload_images":
-            continue
-        artistname.append(dir_name)
-
+    image_dir = "images"
+    artistname = [name for name in os.listdir(image_dir) if name != ".DS_Store"]
     for name_idx, name in enumerate(artistname):
         train_cnt = 0
         try:
             print("---", name)
-            image_dir = input_dir + "/" + name
+            image_dir = image_dir + "/" + name
             files = glob.glob(image_dir + "/*.jpg")
             for file_idx, file in enumerate(files):
-                if train_cnt >= num_traindata:
+                if train_cnt >= num_data:
                     break
                 img = Image.open(file)
                 img = img.convert("RGB")
                 img = img.resize((image_size, image_size))
+
+                # added for no augumentation
                 # rotation
-                for angle in range(-20, 20, 5):
+                for angle in range(-90, 1, 90):
                     img_r = img.rotate(angle)
                     img_np = np.asarray(img_r)
                     train_data.append([img_np, name_idx])
                     train_cnt += 1
                     # inverse
-                    img_trans = img_r.transpose(Image.FLIP_LEFT_RIGHT)
-                    img_np = np.asarray(img_trans)
-                    train_data.append([img_np, name_idx])
-                    train_cnt += 1
-                    if train_cnt >= num_traindata:
+                    # img_trans = img_r.transpose(Image.FLIP_LEFT_RIGHT)
+                    # img_np = np.asarray(img_trans)
+                    # train_data.append([img_np, name_idx])
+                    # train_cnt += 1
+                    if train_cnt >= num_data:
                         break
         except:
             print("SKIP : " + name)
