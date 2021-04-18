08354b0176eeb8b946c64d3668a14ece3888526f
KotaroIwasawa <kotaro1988@gmail.com>
2021-04-18 22:44:08 +0900

    Add information and sample model to the repository

--- web.py
+++ web.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from werkzeug.utils import secure_filename
 import os
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
+
 import judge_artist_name
 
 UPLOAD_FOLDER = "./static/upload_images"
@@ -10,7 +12,6 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(["png", "jpg", "gif", "PNG", "JPG", "GIF"])
 
 
-# ルーティング "/" にアクセス時
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -20,7 +21,6 @@ def allowed_file(file_name):
     return "." in file_name and file_name.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
 
 
-# 画像投稿時のアクション
 @app.route("/post", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
@@ -38,7 +38,7 @@ def upload_file():
             file_path = os.path.join(app.config["UPLOAD_FOLDER"], file_name)
             file.save(file_path)
             (h_indexes, artistname, result_score) = judge_artist_name.main(
-                file_path, "./model/artist-model_15.hdf5"
+                file_path, hdf5_path="./model/artist-model_15.hdf5"
             )
         else:
             return redirect(url_for("index"))
